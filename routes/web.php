2e5e08d92388dbef4539d1633d822a54cbe20616
Marcelo Daudt <mdaudt@yahoo.com.br>
2018-08-23 15:54:33 -0300

    Fixes #2 - Issue 2 resolvida (index)

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
+/*
 Route::get('/', function () {
     return view('welcome');
 });
+ */
+
+Route::get('/', 'DisciplinaController@index');
+Route::resource('disciplinas', 'DisciplinaController');
+
