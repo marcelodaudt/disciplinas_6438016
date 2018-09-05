3dcd9dce6d7769c10fdcc4804a49e265c6fdc076
Marcelo Daudt <mdaudt@yahoo.com.br>
2018-09-05 13:59:48 -0300

    Fix #10 - Listando turmas em show disciplinas

--- resources/views/disciplinas/show.blade.php
+++ resources/views/disciplinas/show.blade.php
@@ -1,3 +1,8 @@
 <h1>{{ $disciplina->titulo }}</h1>
 <p>{{ $disciplina->ementa }}</p>
+@foreach ($disciplina->turmas as $turma)
+    {{ $turma->ministrante }}
+    {{ $turma->inicio }}
+    <br>
+@endforeach
 <p><a href="/disciplinas/{{ $disciplina->id }}/turmas/create">Inserir Turma</a></p>
